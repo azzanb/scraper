a7491dc03ae8d050e67aa7914dd4c60fcdaafb86
Azzan Braxton <azzanbraxton@gmail.com>
2017-08-05 04:31:20 -0400

    T-shirt Site Scraper Update

--- scraper.js
+++ scraper.js
@@ -1,19 +1,87 @@
-var fs = require('fs');
-var http = require('http');
-var request = require('request');
-var csv = require('fast-csv');
-var url = 'http://www.shirts4mike.com/shirts.php';
-var ws = fs.createWriteStream('scraper.csv');
-
-//This gives me the html. I should take this, parse it in a format that's able to be parsed to csv??
-//Or am I missing a step?
-request(url, function(error, response, data){
-  console.log(data);
-});
-
-
+var fs = require('fs'),
+  request = require('request'),
+  cheerio = require('cheerio'),
+  url = 'http://www.shirts4mike.com/shirts.php',
+  mainPage = 'http://www.shirts4mike.com/';
+  ws = fs.createWriteStream('scraper.csv');
 
 //Create 'data' folder if it doesn't exist
 !fs.existsSync('data') ? fs.mkdirSync('data') : false;
+
+//This gives me the html. I should take this, parse it in a format that's able to be parsed to csv??
+//Or am I missing a step?
+request({
+  url,
+  json: true
+}, (error, response, body) => {
+  var $ = cheerio.load(body);
+  
+  //shirt is the object storing the JSON data
+  var shirt = {
+    title: "",
+    price: "",
+    url: "",
+    urlImage: ""
+  };
+  
+//Arrays to store data to be placed inside objects
+  var titleArr = [], 
+    priceArr = [],
+    urlArr = [],
+    urlImageArr = [];
+
+//Grab shirt name
+  var shirtTitle = $('ul.products img').each(function(){
+    var title = $(this).attr().alt;
+    titleArr.push(title);
+    shirt.title = titleArr;
+  });
+
+//Grab URL of shirt
+  var shirtUrl = $('ul.products a').each(function(){
+    var url = $(this).attr().href;
+    urlArr.push(url);
+    shirt.url = urlArr;
+  });
+
+//Grab shirt image URL
+  var shirtImage = $('ul.products img').each(function(){
+    var image = $(this).attr().src;
+    urlImageArr.push(image);
+    shirt.urlImage = urlImageArr;
+  });
+
+//Grab prices
+  const priceURL = shirt.url;
+
+  priceURL.forEach(function(ele){
+    var urls = mainPage + ele;
+    request(urls, (error, response, body) => {
+      var $ = cheerio.load(body);
+      $('span.price').each(function(){
+        var price = $(this).text();
+        console.log(price);
+      })
+    });
+  });
+  
+  
+
+//   fs.writeFile('shirt.csv', JSON.stringify(shirt, null, 2), function(err){
+//     //console.log("Works");
+//   });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
 
 
