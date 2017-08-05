bb5391f59705a32a115750b91add028d89201431
Azzan Braxton <azzanbraxton@gmail.com>
2017-08-05 18:56:27 -0400

    Update

--- scraper.js
+++ scraper.js
@@ -1,80 +1,65 @@
 var fs = require('fs'),
   request = require('request'),
+  json2csv = require('json2csv'),
+  scrape = require('scrape-it'),
   cheerio = require('cheerio'),
   url = 'http://www.shirts4mike.com/shirts.php',
   mainPage = 'http://www.shirts4mike.com/';
   ws = fs.createWriteStream('scraper.csv');
 
+ // const shirtURL = data.shirts[4].shirtImageURL;
+ //        const priceURL = data.shirts;
+ //        priceURL.forEach(function(ele, i){
+          
+ //        });
+
 //Create 'data' folder if it doesn't exist
 !fs.existsSync('data') ? fs.mkdirSync('data') : false;
 
-//This gives me the html. I should take this, parse it in a format that's able to be parsed to csv??
-//Or am I missing a step?
-request({
-  url,
-  json: true
-}, (error, response, body) => {
-  var $ = cheerio.load(body);
-  
-  //shirt is the object storing the JSON data
-  var shirt = {
-    title: "",
-    price: "",
-    url: "",
-    urlImage: ""
-  };
-  
-//Arrays to store data to be placed inside objects
-  var titleArr = [], 
-    priceArr = [],
-    urlArr = [],
-    urlImageArr = [];
+scrape(url, 
+  { 
+    shirts: {
+      listItem: ".products li",
+      data: {
+        'title': {
+          selector:"img",
+          attr: "alt"
+        },
+        'price': {
+          selector: "span.price"
 
-//Grab shirt name
-  var shirtTitle = $('ul.products img').each(function(){
-    var title = $(this).attr().alt;
-    titleArr.push(title);
-    shirt.title = titleArr;
+        },
+        'shirtURL': {
+          selector: "a",
+          attr: "href", 
+          convert: x => mainPage + x
+        },
+        'shirtImageURL': {
+          selector: "img",
+          attr: "src",
+          convert: x => mainPage + x
+        }
+      }
+    } 
+}).then(data => {
+    const arr = data.shirts;
+    //console.log(arr);
+    arr.forEach(function(ele, i){
+      const fields = ['ele.title', 'ele.shirtURL', 'ele.shirtImageURL'];
+      const fieldNames = ["Shirt Name", "Shirt URL", "Shirt Image URL"];
+      const csv = json2csv({data: arr, fields: fields, fieldNames: fieldNames});
+      console.log(csv);
+     
+     // fs.writeFile('shirt.csv', csv, function(err){
+     //  if(err) throw err;
+     //  console.log("Works");
+     // });
+    });  
   });
+    
 
-//Grab URL of shirt
-  var shirtUrl = $('ul.products a').each(function(){
-    var url = $(this).attr().href;
-    urlArr.push(url);
-    shirt.url = urlArr;
-  });
 
-//Grab shirt image URL
-  var shirtImage = $('ul.products img').each(function(){
-    var image = $(this).attr().src;
-    urlImageArr.push(image);
-    shirt.urlImage = urlImageArr;
-  });
-
-//Grab prices -- PROBLEM IS THIS SECTION
-  const priceURL = shirt.url;
-
-  priceURL.forEach(function(ele){
-    var urls = mainPage + ele;
-    request(urls, (error, response, body) => {
-      var $ = cheerio.load(body);
-      $('span.price').each(function(){
-        var price = $(this).text();
-        console.log(price);
-      })
-    });
-  });
   
-  
-
-//   fs.writeFile('shirt.csv', JSON.stringify(shirt, null, 2), function(err){
-//     //console.log("Works");
-//   });
-// });
-
-
-
-
 
 
 
